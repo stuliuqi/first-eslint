e8f2789bf8cdc9ae72168f76691c7ad61f757f0b
stuliuqi <18qliu1@stu.edu.cn>
2019-02-06 18:03:48 +0800

    adas

---
+++ eslint-exercise.js
@@ -0,0 +1,37 @@
+const timeout = ms => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve();
+    }, ms);
+});
+
+const ajax1 = () => timeout(2000).then(() => {
+    console.log('1');
+    return 1;
+});
+
+const ajax2 = () => timeout(1000).then(() => {
+    console.log('2');
+    return 2;
+});
+
+const ajax3 = () => timeout(2000).then(() => {
+    console.log('3');
+    return 3;
+});
+
+const mergePromise = ajaxArray => {
+    async function controller(ajaxArray) {
+        num = [];
+        for (i = 0; i < ajaxArray.length; i++) {
+            num[i] = await ajaxArray[i]();
+        }
+        return num;
+    }
+    return controller(ajaxArray);
+
+};
+
+mergePromise([ajax1, ajax2, ajax3]).then(data => {
+    console.log('done');
+    console.log(data); // data 为 [1, 2, 3]
+});
